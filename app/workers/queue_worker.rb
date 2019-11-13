a68f693b7a63b6373c65cdf995300b003939f24f
Niklas Paulsson <niklasp@gmail.com>
2019-11-13 22:53:57 +0100

    mod

--- app/workers/queue_worker.rb
+++ app/workers/queue_worker.rb
@@ -3,12 +3,13 @@ b.start
 ch = b.create_channel
 queue = ch.queue('customer_queue', durable: true)
 
-queue.subscribe(manual_ack: false, block: true) do |_delivery_info, properties, payload|
+queue.subscribe(manual_ack: false, block: true) do |_delivery_info, _properties, payload|
   c = Customer.new
   c.from_json(payload)
   c.approved = false
   if c.save
-    n = GELF::Notifier.new('zaklog1', 12_201)
-    n.notify!(short_message: "Imported new user #{name}", full_message: c.to_json)
+    Rails.configuration.notifier.notify!(short_message: "Imported new user #{name}",
+                                         full_message: c.to_json)
+    ack!
   end
 end
