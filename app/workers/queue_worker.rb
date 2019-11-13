2fb4bd382eaa5edae9d75f047a9a93e71f147928
Niklas Paulsson <niklasp@gmail.com>
2019-11-13 23:19:05 +0100

    mod

--- app/workers/queue_worker.rb
+++ app/workers/queue_worker.rb
@@ -4,12 +4,11 @@ ch = b.create_channel
 queue = ch.queue('customer_queue', durable: true)
 
 queue.subscribe(manual_ack: false, block: true) do |_delivery_info, _properties, payload|
+  Rails.logger.info('Queue event')
   c = Customer.new
   c.from_json(payload)
   c.approved = false
   if c.save
-    Rails.configuration.notifier.notify!(short_message: "Imported new user #{name}",
-                                         full_message: c.to_json)
-    ack!
+    Rails.logger.info(short_message: "Imported new user #{c.name}", full_message: c.to_json)
   end
 end
